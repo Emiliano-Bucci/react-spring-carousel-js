e4c3071c0a34a23cbcf9c2ee2168628039b8caf0
Emiliano-Bucci <emiliano.bucci@outlook.com>
2020-12-05 18:43:42 +0100

    feat: add carousel wrappers and basic interaction

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Wrapper } from './index.styles'
+import React, { useState } from 'react'
+import { useSpring } from 'react-spring'
+import { Wrapper, CarouselWrapper, CarouselItemWrapper } from './index.styles'
 
 interface Item {
   id: string
@@ -11,6 +12,54 @@ interface Props<T extends Item> {
 }
 
 export function ReactSpringCarousel<T extends Item>({ items }: Props<T>) {
-  console.log(items)
-  return <Wrapper>asdasda</Wrapper>
+  const [activeItem, setActiveItem] = useState(0)
+  const [carouselStyles, setCarouselStyles] = useSpring(() => ({
+    x: 0
+  }))
+
+  console.log({
+    activeItem,
+    setActiveItem
+  })
+
+  function handleSlideToNextItem() {
+    setCarouselStyles({
+      x: 1
+    })
+  }
+
+  return (
+    <Wrapper>
+      <div
+        style={{
+          position: 'relative',
+          background: 'blue',
+          zIndex: 100
+        }}
+      >
+        prev item
+      </div>
+      <CarouselWrapper
+        style={{
+          transform: carouselStyles.x.to(
+            (value) => `translateX(${value * 100}%)`
+          )
+        }}
+      >
+        {items.map(({ id, renderItem }) => (
+          <CarouselItemWrapper key={id}>{renderItem}</CarouselItemWrapper>
+        ))}
+      </CarouselWrapper>
+      <div
+        style={{
+          position: 'relative',
+          background: 'blue',
+          zIndex: 100
+        }}
+        onClick={handleSlideToNextItem}
+      >
+        next item
+      </div>
+    </Wrapper>
+  )
 }
