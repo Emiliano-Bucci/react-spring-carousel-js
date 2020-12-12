574d52e6f635c3c82a51fd4eb37c570565e417f7
Emiliano-Bucci <emiliano.bucci@outlook.com>
2020-12-12 12:15:45 +0100

    feat: add event emitters and fullscreen module to fading carousel

--- src/carouselTypes/useFadingCarousel.tsx
+++ src/carouselTypes/useFadingCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { createContext, useRef, useState } from 'react'
 import {
   useTransition,
   SpringConfig,
@@ -7,9 +7,21 @@ import {
   TransitionFrom,
   TransitionTo
 } from 'react-spring'
-import { ReactSpringCarouselItem } from '../types'
+import { prepareDataForCustomEvent } from '../index.utils'
+import {
+  ListenToCustomEvent,
+  useCustomEventsModule
+} from '../modules/useCustomEventsModule'
+import { useFullscreenModule } from '../modules/useFullscreenModule'
+import {
+  RCSJSOnSlideChange,
+  RCSJSOnSlideStartChange,
+  ReactSpringCarouselItem,
+  ReactSpringCustomEvents
+} from '../types'
 
 type SpringAnimationProps<Item> = {
+  initial: TransitionFrom<Item>
   from: TransitionFrom<Item>
   enter: TransitionTo<Item>
   leave: TransitionTo<Item>
@@ -23,18 +35,38 @@ type FadingCarouselProps<T extends ReactSpringCarouselItem> = {
   withLoop?: boolean
 }
 
+type FadingCarouselContextProps = {
+  activeItem: number
+  slideToNextItem(): void
+  slideToPrevItem(): void
+  enterFullscreen(elementRef?: HTMLElement): void
+  exitFullscreen(): void
+  slideToItem(item: number): void
+  getIsAnimating(): boolean
+  getIsPrevItem(id: string): boolean
+  getIsNextItem(id: string): boolean
+  useListenToCustomEvent: ListenToCustomEvent
+}
+
+const FadingCarouselContext = createContext<FadingCarouselContextProps>({
+  activeItem: 0,
+  slideToNextItem: () => {},
+  slideToPrevItem: () => {},
+  enterFullscreen: () => {},
+  exitFullscreen: () => {},
+  slideToItem: () => {},
+  getIsAnimating: () => false,
+  getIsPrevItem: () => false,
+  getIsNextItem: () => false,
+  useListenToCustomEvent: () => {}
+})
+
 export function useFadingCarousel<T extends ReactSpringCarouselItem>({
   items,
   withLoop = true,
   // withThumbs = true,
-  springConfig = config.default
-}: FadingCarouselProps<T>) {
-  const mainCarouselWrapperRef = useRef<HTMLDivElement | null>(null)
-  const [activeItem, setActiveItem] = useState(0)
-
-  const transitions = useTransition(activeItem, {
-    config: springConfig,
-    key: () => items[activeItem].id,
+  springConfig = config.default,
+  springAnimationPops = {
     initial: {
       opacity: 1
     },
@@ -42,12 +74,42 @@ export function useFadingCarousel<T extends ReactSpringCarouselItem>({
       opacity: 0
     },
     enter: {
-      opacity: 1,
-      position: 'relative'
+      opacity: 1
     },
     leave: {
       opacity: 0,
       position: 'absolute'
+    }
+  }
+}: FadingCarouselProps<T>) {
+  const mainCarouselWrapperRef = useRef<HTMLDivElement | null>(null)
+  const isAnimating = useRef(false)
+  const [activeItem, setActiveItem] = useState(0)
+
+  const { emitCustomEvent, useListenToCustomEvent } = useCustomEventsModule()
+  const { enterFullscreen, exitFullscreen } = useFullscreenModule({
+    emitCustomEvent,
+    mainCarouselWrapperRef
+  })
+
+  // @ts-ignore
+  const transitions = useTransition(activeItem, {
+    config: springConfig,
+    key: () => items[activeItem].id,
+    ...springAnimationPops,
+    onStart: () => {
+      isAnimating.current = true
+    },
+    onRest: () => {
+      isAnimating.current = false
+      emitCustomEvent(
+        ReactSpringCustomEvents['RCSJS:onSlideChange'],
+        prepareDataForCustomEvent<RCSJSOnSlideChange>({
+          prevItem: getPrevItem(),
+          currentItem: activeItem,
+          nextItem: getNextItem()
+        })
+      )
     }
   })
   const itemsFragment = transitions((styles, item) => (
@@ -63,18 +125,46 @@ export function useFadingCarousel<T extends ReactSpringCarouselItem>({
     </animated.div>
   ))
 
+  function getPrevItem() {
+    if (activeItem === 0) {
+      return items.length - 1
+    }
+
+    return activeItem
+  }
+
+  function getNextItem() {
+    if (activeItem === items.length - 1) {
+      return 0
+    }
+
+    return activeItem
+  }
+
+  function slideToItem(item: number) {
+    emitCustomEvent(
+      ReactSpringCustomEvents['RCSJS:onSlideStartChange'],
+      prepareDataForCustomEvent<RCSJSOnSlideStartChange>({
+        prevItem: getPrevItem(),
+        currentItem: activeItem,
+        nextItem: getNextItem()
+      })
+    )
+    setActiveItem(item)
+  }
+
   function slideToNextItem() {
     const isLastItem = activeItem === items.length - 1
 
     if (withLoop) {
       if (isLastItem) {
-        setActiveItem(0)
+        slideToItem(0)
       } else {
-        setActiveItem((prev) => prev + 1)
+        slideToItem(activeItem + 1)
       }
     } else {
       if (!isLastItem) {
-        setActiveItem((prev) => prev + 1)
+        slideToItem(activeItem + 1)
       }
     }
   }
@@ -84,35 +174,77 @@ export function useFadingCarousel<T extends ReactSpringCarouselItem>({
 
     if (withLoop) {
       if (isFirstItem) {
-        setActiveItem(items.length - 1)
+        slideToItem(items.length - 1)
       } else {
-        setActiveItem((prev) => prev - 1)
+        slideToItem(activeItem - 1)
       }
     } else {
       if (!isFirstItem) {
-        setActiveItem((prev) => prev - 1)
+        slideToItem(activeItem - 1)
       }
     }
   }
 
+  function findItemIndex(id: string) {
+    return items.findIndex((item) => item.id === id)
+  }
+
+  function getIsNextItem(id: string) {
+    const itemIndex = findItemIndex(id)
+
+    if (withLoop && activeItem === items.length - 1) {
+      return itemIndex === 0
+    }
+
+    return itemIndex === activeItem + 1
+  }
+
+  function getIsPrevItem(id: string) {
+    const itemIndex = findItemIndex(id)
+
+    if (withLoop && activeItem === 0) {
+      return itemIndex === items.length - 1
+    }
+
+    return itemIndex === activeItem - 1
+  }
+
+  function getIsAnimating() {
+    return isAnimating.current
+  }
+
+  const contextProps: FadingCarouselContextProps = {
+    activeItem,
+    slideToItem,
+    slideToNextItem,
+    slideToPrevItem,
+    enterFullscreen,
+    exitFullscreen,
+    useListenToCustomEvent,
+    getIsNextItem,
+    getIsPrevItem,
+    getIsAnimating
+  }
+
   const carouselFragment = (
-    <div
-      ref={mainCarouselWrapperRef}
-      style={{
-        display: 'flex',
-        position: 'relative',
-        width: '100%',
-        height: '100%',
-        overflow: 'hidden'
-      }}
-    >
-      {itemsFragment}
-    </div>
+    <FadingCarouselContext.Provider value={contextProps}>
+      <div
+        ref={mainCarouselWrapperRef}
+        style={{
+          display: 'flex',
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden'
+        }}
+      >
+        {itemsFragment}
+      </div>
+    </FadingCarouselContext.Provider>
   )
 
   return {
     carouselFragment,
-    slideToNextItem,
-    slideToPrevItem
+    ...contextProps
   }
 }
--- src/carouselTypes/useTransformCarousel.tsx
+++ src/carouselTypes/useTransformCarousel.tsx
@@ -195,11 +195,23 @@ export function useTransformCarousel<T extends ReactSpringCarouselItem>({
   }
 
   function getPrevItem() {
-    return getCurrentActiveItem() - 1
+    const currentActiveItem = getCurrentActiveItem()
+
+    if (currentActiveItem === 0) {
+      return items.length - 1
+    }
+
+    return currentActiveItem - 1
   }
 
   function getNextItem() {
-    return getCurrentActiveItem() + 1
+    const currentActiveItem = getCurrentActiveItem()
+
+    if (currentActiveItem === items.length - 1) {
+      return 0
+    }
+
+    return currentActiveItem + 1
   }
 
   function slideToItem({
@@ -340,6 +352,28 @@ export function useTransformCarousel<T extends ReactSpringCarouselItem>({
     return items.findIndex((item) => item.id === id)
   }
 
+  function getIsNextItem(id: string) {
+    const itemIndex = findItemIndex(id)
+    const activeItem = getCurrentActiveItem()
+
+    if (withLoop && activeItem === items.length - 1) {
+      return itemIndex === 0
+    }
+
+    return itemIndex === activeItem + 1
+  }
+
+  function getIsPrevItem(id: string) {
+    const itemIndex = findItemIndex(id)
+    const activeItem = getCurrentActiveItem()
+
+    if (withLoop && activeItem === 0) {
+      return itemIndex === items.length - 1
+    }
+
+    return itemIndex === activeItem - 1
+  }
+
   const contextProps: TransformCarouselContextProps = {
     useListenToCustomEvent,
     getIsFullscreen,
@@ -347,8 +381,8 @@ export function useTransformCarousel<T extends ReactSpringCarouselItem>({
     exitFullscreen,
     getIsAnimating,
     getIsDragging,
-    getIsNextItem: (id) => findItemIndex(id) - 1 === getCurrentActiveItem(),
-    getIsPrevItem: (id) => findItemIndex(id) - 1 === getCurrentActiveItem() - 2,
+    getIsNextItem,
+    getIsPrevItem,
     getIsActiveItem: (id) => findItemIndex(id) === getCurrentActiveItem(),
     slideToPrevItem,
     slideToNextItem,
