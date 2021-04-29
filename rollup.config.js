2590ae9cda39999c5b128632135b069d04a60add
Emiliano-Bucci <emiliano.bucci@outlook.com>
2021-04-29 10:21:09 +0200

    feat: generate different bundle for every carousel type

--- rollup.config.js
+++ rollup.config.js
@@ -4,42 +4,55 @@ import external from 'rollup-plugin-peer-deps-external'
 import rollupTS from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
 import commonjs from '@rollup/plugin-commonjs'
+import size from 'rollup-plugin-filesize'
+import visualizer from 'rollup-plugin-visualizer'
 
-const globals = {
-  react: 'React',
-  'react-dom': 'ReactDOM',
-  'react/jsx-runtime': 'jsxRuntime',
-  'react-spring': 'reactSpring',
-  'react-use-gesture': 'reactUseGesture',
-  rxjs: 'rxjs',
-  screenfull: 'screenfull',
-}
-
-export default {
-  input: 'src/index.tsx',
-  output: [
-    {
-      format: 'umd',
-      exports: 'named',
-      dir: 'dist/',
-      sourcemap: true,
-      name: 'ReactSpringCarousel',
-      globals,
+const plugins = [
+  rollupTS({
+    tsconfigOverride: {
+      exclude: ['Examples', 'node_modules'],
     },
-  ],
-  plugins: [
-    rollupTS({
-      tsconfigOverride: {
-        exclude: ['Examples', 'node_modules'],
+  }),
+  babel({
+    exclude: 'node_modules/**',
+    presets: ['@babel/preset-react'],
+  }),
+  external(),
+  resolve(),
+  commonjs(),
+  terser(),
+  size(),
+  visualizer({
+    filename: 'stats-react.json',
+    json: true,
+  }),
+]
+
+export default [
+  {
+    input: 'src/carouselTypes/useSpringCarousel.tsx',
+    output: [
+      {
+        format: 'cjs',
+        exports: 'named',
+        dir: 'dist/',
+        sourcemap: true,
+        name: 'ReactSpringCarousel',
       },
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      presets: ['@babel/preset-react'],
-    }),
-    external(),
-    resolve(),
-    commonjs(),
-    terser(),
-  ],
-}
+    ],
+    plugins,
+  },
+  {
+    input: 'src/carouselTypes/useTransitionCarousel.tsx',
+    output: [
+      {
+        format: 'cjs',
+        exports: 'named',
+        dir: 'dist/',
+        sourcemap: true,
+        name: 'ReactSpringCarousel',
+      },
+    ],
+    plugins,
+  },
+]
