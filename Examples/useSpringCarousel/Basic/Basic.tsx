0d0430c6c664ded31bc6b6c1f56a5c6078803d08
Emiliano-Bucci <emiliano.bucci@outlook.com>
2021-05-10 17:41:48 +0200

    fix: restore basic storybook example

--- Examples/useSpringCarousel/Basic/Basic.tsx
+++ Examples/useSpringCarousel/Basic/Basic.tsx
@@ -2,61 +2,13 @@ import { useSpringCarousel } from '../../../src'
 import { mockedItems } from '../mocked-data'
 import { SliderItem } from '../../components/SliderItem/SliderItem'
 import { SliderWrapper } from '../../components/SliderWrapper/SliderWrapper'
-import { css } from '@emotion/react'
 
 export function Basic() {
-  const {
-    carouselFragment,
-    thumbsFragment,
-    slideToNextItem,
-    slideToPrevItem,
-  } = useSpringCarousel({
-    withLoop: true,
-    withThumbs: true,
+  const { carouselFragment } = useSpringCarousel({
     items: mockedItems.map(({ id, label, ...rest }) => ({
       id,
       renderItem: <SliderItem {...rest}>{label}</SliderItem>,
-      renderThumb: (
-        <div
-          css={css`
-            width: 400px;
-            height: 200px;
-            ${rest.css}
-          `}
-        >
-          THUMB {id}
-        </div>
-      ),
     })),
   })
-  return (
-    <SliderWrapper>
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          height: 100%;
-        `}
-      >
-        <div
-          css={css`
-            flex: 1;
-            display: flex;
-          `}
-        >
-          <button onClick={slideToPrevItem}>PREV</button>
-          {carouselFragment}
-          <button onClick={slideToNextItem}>NEXT</button>
-        </div>
-        <div
-          css={css`
-            flex: 1;
-            overflow: hidden;
-          `}
-        >
-          {thumbsFragment}
-        </div>
-      </div>
-    </SliderWrapper>
-  )
+  return <SliderWrapper>{carouselFragment}</SliderWrapper>
 }
