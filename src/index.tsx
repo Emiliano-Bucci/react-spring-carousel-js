25238648dc2c626f111dcf802163522c0901fd98
Emiliano-Bucci <emiliano.bucci@outlook.com>
2021-05-12 14:47:42 +0200

    fix: fixed missing context export

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
 export { default as useSpringCarousel } from './useSpringCarousel'
 export { default as useTransitionCarousel } from './useTransitionCarousel'
+export * from './useSpringCarousel'
+export * from './useTransitionCarousel'
 export * from './types'
